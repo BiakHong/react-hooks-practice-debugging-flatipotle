4c5ed1186a6894b2c1821ea810b33682424aea11
Lian <biakhong@gmail.com>
2022-06-18 16:25:35 -0400

    Completed Lab

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,20 +11,22 @@ const DEFAULT_STATE = {
   sides: [],
 };
 
-function Form() {
+function Form({addOrder}) {
   const [formState, setFormState] = useState(DEFAULT_STATE);
 
-  function handleSubmit() {
+  function handleSubmit(event) {
     event.preventDefault();
-    props.addOrder(formState);
+    
 
     setFormState({
       ...DEFAULT_STATE,
     });
+    addOrder(formState);
     event.target.reset();
+    
   }
 
-  function handleChange() {
+  function handleChange(event) {
     const itemType = event.target.name;
     const item = event.target.value;
 
@@ -47,20 +49,20 @@ function Form() {
       <form className="ui form" id="order-form" onSubmit={handleSubmit}>
         <ProteinForm
           protein={formState.protein}
-          handleOnChange={handleChange}
+          handleChange={handleChange}
         />
 
         <FillingForm
           fillings={formState.fillings}
-          handleOnChange={handleChange}
+          handleChange={handleChange}
         />
 
         <ToppingForm
           toppings={formState.toppings}
-          handleOnChange={handleChange}
+          handleChange={handleChange}
         />
 
-        <SideForm sides={formState.sides} handleOnChange={handleChange} />
+        <SideForm sides={formState.sides} handleChange={handleChange} />
 
         <br />
 
